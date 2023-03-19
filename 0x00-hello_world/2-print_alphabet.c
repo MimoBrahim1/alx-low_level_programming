3cca9788857a596270eb081e958dae0a708c04b4
MimoBrahim1 <younesboujmira@gmail.com>
2023-03-19 09:05:29 -0700

    I sometimes suffer from insomnia

---
+++ 0x00-hello_world/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main-prints the alphabet in lowercase, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char alph = 'a';
+
+int main () {
+   char ch;
+
+   for(ch = 'a' ; ch <= 'z' ; ch++) {
+      putchar(ch);
+   }
+
+    putchar ('\n');
+
+   return(0);
+	while (alph <= 'z')
+	{
+	putchar (alph);
+	alph++;
+	}
+	putchar ('\n');
+	return (0);
+}
